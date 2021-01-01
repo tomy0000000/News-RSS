255263e05612f4240cf6e45dbd57f932c63e0c46
Tomy Hsieh <tomy0000000@gmail.com>
2021-01-01 14:50:13 +0800

    Minor Improvements
    * Format timestamp with RFC 2822
    * Escape item data with CDATA
    * Handling empty fields
    * Sort item with timestamp
    * Modify default scaping behavior
    * Improve parser of appledaily

--- news/exporters.py
+++ news/exporters.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+from email.utils import format_datetime
+
 from scrapy.exporters import XmlItemExporter
 
-
-VALID_RSS_ELEMENT = {
+VALID_RSS_ELEMENTS = {
     "channel": [
         "category",
         "cloud",
@@ -23,25 +25,35 @@ VALID_RSS_ELEMENT = {
         "ttl",
         "webMaster",
     ],
-    "item": [
-        "author",
-        "category",
-        "comments",
-        "description",
-        "enclosure",
-        "guid",
-        "link",
-        "pubDate",
-        "source",
-        "title",
-    ],
+    "item": {  # value: should this field be escaped
+        "author": True,
+        "category": True,
+        "comments": True,
+        "description": True,
+        "enclosure": False,
+        "guid": False,
+        "link": False,
+        "pubDate": False,
+        "source": True,
+        "title": True,
+    },
 }
+
+# FIXME: guid isPermaLink attributes
+# https://www.w3schools.com/xml/rss_tag_guid.asp
+
+# FIXME: source url attributes
+# https://www.w3schools.com/xml/rss_tag_source.asp
+
+# FIXME: Add support for enclosure
+# https://www.w3schools.com/xml/rss_tag_enclosure.asp
 
 
 class RSSExporter(XmlItemExporter):
     def __init__(self, file, channel_meta, **kwargs):
         self.file = file
         self.channel_meta = channel_meta
+        self.item_list = []
         super().__init__(file, root_element="channel", **kwargs)
 
     def start_exporting(self):
@@ -59,18 +71,38 @@ class RSSExporter(XmlItemExporter):
             self._export_xml_field(field, value, depth=2)
 
     def export_item(self, item):
-        # Same as the parent class, just modifying indent depth to 2
+        # Didn't actually write to file, store to list and write after sorting
+        self.item_list.append(item)
+
+    def write_item(self, item):
+        # Edit from `export_item` from `scrapy.exporters.XmlItemExporter`
+        # Add indent depth from 2 to 3
         self._beautify_indent(depth=2)
         self.xg.startElement(self.item_element, {})
         self._beautify_newline()
         for name, value in self._get_serialized_fields(item, default_value=""):
-            if name in VALID_RSS_ELEMENT["item"]:
-                self._export_xml_field(name, value, depth=3)
+            if name in VALID_RSS_ELEMENTS["item"]:
+                # Cleanup data
+                if isinstance(value, datetime):  # Format timestamp
+                    value = format_datetime(value)
+                elif value is None:  # Fill empty field with empty string
+                    value = ""
+
+                # Write to xml
+                if VALID_RSS_ELEMENTS["item"][name]:  # should this field be escaped
+                    self._export_xml_field(name, f"<![CDATA[{value}]]>", depth=3)
+                else:
+                    self._export_xml_field(name, value, depth=3)
         self._beautify_indent(depth=2)
         self.xg.endElement(self.item_element)
         self._beautify_newline(new_item=True)
 
     def finish_exporting(self):
+        # Sort and write items
+        self.item_list.sort(key=lambda x: x["pubDate"], reverse=True)
+        for item in self.item_list:
+            self.write_item(item)
+
         # channel tag
         self._beautify_indent(depth=1)
         self.xg.endElement(self.root_element)
--- news/spiders/appledaily.py
+++ news/spiders/appledaily.py
@@ -1,6 +1,9 @@
-import scrapy
-from datetime import datetime
+from datetime import datetime, timedelta
 
+import scrapy
+from pytz import timezone
+
+TIMEZONE = "Asia/Taipei"
 CATEGORIES = {
     "headline": "要聞",
     "entertainment": "娛樂",
@@ -26,30 +29,60 @@ class AppleDailySpider(scrapy.Spider):
         "title": "蘋果新聞網",
     }
 
-    def __init__(self, date=datetime.today().strftime("%Y%m%d"), *args, **kwargs):
+    def __init__(self, date=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.start_urls = [f"https://tw.appledaily.com/archive/{date}/"]
-        self.file_name = f"appledaily_{date}"  # Custom, used in pipeline
+        if date is None:
+            self.start_urls = ["https://tw.appledaily.com/archive/"]
+            self.crawl_one_more_page = True
+            self.file_name = "appledaily"
+        else:
+            self.start_urls = [f"https://tw.appledaily.com/archive/{date}/"]
+            self.crawl_one_more_page = False
+            self.file_name = f"appledaily_{date}"  # Custom, used in pipeline
 
     def parse(self, response, **kwargs):
         news_links = response.xpath("//*[@id='section-body']/div/a")
         yield from response.follow_all(news_links, self.parse_news)
+        if self.crawl_one_more_page:
+            one_day_before = (
+                datetime.strptime(
+                    response.xpath(
+                        "//*[@id='section-body']/div[2]/div[2]/span/text()"
+                    ).get(),
+                    "%Y.%m.%d",
+                )
+                - timedelta(days=1)
+            )
+            self.crawl_one_more_page = False
+            yield scrapy.Request(
+                f"https://tw.appledaily.com/archive/{one_day_before.strftime('%Y%m%d')}/"
+            )
 
     def parse_news(self, response):
         url = response.url
-        context_selector = response.xpath("//*[@id='articleBody']/section[2]/p/text()")
+        context_selector = response.xpath("//*[@id='articleBody']/section[2]/p")
+        for sel in context_selector:
+            self.logger.info(
+                "".join(sel.xpath("descendant-or-self::*/text()").getall())
+            )
         yield {
             "url": url,
             "title": response.xpath(
                 "//*[@id='article-header']/header/div/h1/span/text()"
             ).get(),
-            "context": "\n".join(context_selector.getall()),
+            "context": "\n".join(
+                [
+                    "".join(sel.xpath("descendant-or-self::*/text()").getall())
+                    for sel in context_selector
+                ]
+            ),
             "author": context_selector.re_first(r"【(.*)】"),
             "category": CATEGORIES.get(url.split("/")[-4]),
             "id": url.split("/")[-2],
-            "timestamp": response.xpath(
-                "//*[@id='article-header']/div/div/text()"
-            ).getall()[1],
+            "timestamp": datetime.strptime(
+                response.xpath("//*[@id='article-header']/div/div/text()").getall()[1],
+                "%Y/%m/%d %H:%M",
+            ).astimezone(timezone(TIMEZONE)),
             "third_party": context_selector.re_first(r"本文由(.*)提供"),
             "subtitle": response.xpath(
                 "//*[@id='article-header']/header/p/span/text()"
